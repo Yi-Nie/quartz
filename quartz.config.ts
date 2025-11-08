cbe9b9c36db82b8df14c06343b3bb2921e46919b
Yi Nie <yi.nie@eonerc.rwth-aachen.de>
2025-11-08 21:33:57 +0100

    modify the configuration file for personal project

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "yi-nie.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
